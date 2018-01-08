12cb199e934947002a6945231ca48755b1157cf6
Matthew Woolstencroft <fusionfox.uk@gmail.com>
2018-01-08 21:52:52 +0000

    client: Setup react-router

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 import './index.css'
 import store from './js/store'
 import ProductSelection from './js/ProductSelection'
 
-ReactDOM.render(<Provider store={store}>
-  <ProductSelection />
-</Provider>, document.getElementById('root'))
+ReactDOM.render(
+  <Provider store={store}>
+    <Router>
+      <Route path='/' component={ProductSelection} />
+    </Router>
+  </Provider>,
+  document.getElementById('root')
+)
